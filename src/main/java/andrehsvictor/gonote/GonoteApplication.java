75b140ffa1c732d2aa2e22e952e8829b088f1075
andrehsvictor <andrehsvictor@gmail.com>
2025-01-22 21:01:31 -0300

    feat: initialize project structure with Spring Boot, PostgreSQL, and Redis configuration

---
+++ src/main/java/andrehsvictor/gonote/GonoteApplication.java
@@ -0,0 +1,13 @@
+package andrehsvictor.gonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andrehsvictor/gonote/GonoteApplicationTests.java
@@ -0,0 +1,13 @@
+package andrehsvictor.gonote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
